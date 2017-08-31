8be6606c355be861ba0149c3cc3d8c1201e10787
Marc Edwards <mdouglasedwards@gmail.com>
2017-08-31 19:37:13 -0400

    refactor: Add all routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, browserHistory } from 'react-router';
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
-	<BrowserRouter><App /></BrowserRouter>,
-	document.getElementById('root')
+	<BrowserRouter history={browserHistory}>
+		<Route path="/" component={App}>
+		</Route>
+	</BrowserRouter>
+	, document.getElementById('root')
 )

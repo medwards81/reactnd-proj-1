adff7c2f8a4e3824b3c52c7b465db2cd6e69e095
Marc Edwards <mdouglasedwards@gmail.com>
2017-08-31 21:21:32 -0400

    refactor: Create Booklist and Seach components, show based on URL route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, browserHistory } from 'react-router';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
-	<BrowserRouter history={browserHistory}>
-		<Route path="/" component={App}>
-		</Route>
-	</BrowserRouter>
-	, document.getElementById('root')
+	<BrowserRouter><App /></BrowserRouter>,
+	document.getElementById('root')
 )
